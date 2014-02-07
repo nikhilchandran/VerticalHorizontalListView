3d08d48af2ebcdc14916fed46f7dc3aedf9ab3ca
NikhilChandran <nikhil@tiltedu.com>
2014-02-07 12:50:21 +0530

    Inital commit

---
+++ gen/com/example/verticalhorizontallistview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.verticalhorizontallistview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
